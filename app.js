f03c03acc6f86b761b528e776a08025012c65995
CarliFP <carlafreschpons@gmail.com>
2021-02-06 23:06:38 -0300

    html e img

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000, () => {
+/*app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000'); 
-})
+})*/
+
+
+app.listen (3000, () => {
+    console.log ("Levantando un servidor con Express: Puerto 3000")
+});
 
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./views/index.html'));
+});
+
+
+app.get('/notas', (req, res) => {
+    res.sendFile(path.resolve('./views/notas.html'));
 });
 
 app.get('/404', (req, res) => {
